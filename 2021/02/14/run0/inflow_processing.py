fc5df685138a10149068a4a96e9cf73e96a76c9d
egdeist <20954092+egdeist@users.noreply.github.com>
2021-02-16 20:40:04 -0800

    Feb 14 run0 inflow processing with piezo scan ROIs

--- 2021/02/14/run0/inflow_processing.py
+++ 2021/02/14/run0/inflow_processing.py
@@ -17,11 +17,12 @@ daily_subpath = Path(*Path.cwd().parent.parts[-3:])
 daily_path = Path(data_root, daily_subpath)
 
 run_name = 'run0'
-num_points = 6
-run_doc_string = ('molasses freq = [4, 4, 4.5, 5, 5.5, 6, 6.25, 6.5, 6.75, 7, 7.2, 7.4, 7.6, 7.8, 8], molasses amp = 4.0 V  (intensity feedback),'
-                  'tweezer_freq_list = [107,108,109,110,111,112,113,114,115,116], wavegen ampitude = 350, 10 tweezers calibrated version 2, molasses on '
-                  '(microtrap int = 1V + molasses no t_hold, NOT flashing the molasses w/ VCO'
-                 'cav_odt_att = [1,2,3,4,5,6] '
+num_points = 40
+run_doc_string = ('molasses freq = [4.5,5,5.5,6,6.5,7,7.5,8]'
+                  'tweezer_freq_list = [108,110,112,114,116], 5 tweezers '
+                  'cav_odt_att = 0 '
+                  'probe att list = [0.1,1,2,3]'
+                  'pzt_para_list = [5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]'
                  't_exposure = 500 ms , t_hold = 100 ms'
                  'photoassociation -  drop cavity ODT - hold - load cavity ODT ')
 
@@ -29,6 +30,7 @@ datamodel = get_datamodel(run_name=run_name, num_points=num_points,
                           run_doc_string=run_doc_string)
 # datamodel = DataModel(run_name=run_name, num_points=num_points,
 #                       run_doc_string=run_doc_string)
+
 datafield_list = []
 processor_list = []
 aggregator_list = []
@@ -37,24 +39,20 @@ reporter_list = []
 high_na_datastream = DataStream(name='high NA Imaging', daily_path=daily_path, run_name=run_name,
                                 file_prefix='jkam_capture')
 
-tweezer_00_vert_center = 52
-tweezer_00_horiz_center = 14
+tweezer_00_vert_center = 81.15
+tweezer_00_horiz_center = 18.2
 tweezer_horiz_span = 14
 tweezer_vert_span = 12
 
-tweezer_vert_spacing = 27.8
-tweezer_horiz_spacing = 1.3867
+tweezer_vert_spacing = 27.8*2
+tweezer_horiz_spacing = 1.3867*2
 
-num_tweezers = 10
-tweezer_roi_list = []
-for tweezer_num in range(num_tweezers):
-    tweezer_roi = make_centered_roi(vert_center=tweezer_00_vert_center + tweezer_num * tweezer_vert_spacing,
-                                    horiz_center=tweezer_00_horiz_center + tweezer_num * tweezer_horiz_spacing,
-                                    vert_span=tweezer_vert_span,
-                                    horiz_span=tweezer_horiz_span)
-    tweezer_roi_list.append(tweezer_roi)
+piezo_vert_shift = 0.3*0.5
+piezo_horiz_shift = 1.4*0.5
 
-num_frames = 17
+num_tweezers = 5
+num_frames = 10
+
 threshold_processor_list = []
 for frame_num in range(num_frames):
     frame_key = f'frame-{frame_num:02d}'
@@ -71,9 +69,15 @@ for frame_num in range(num_frames):
 
     roi_array = np.zeros([num_points, num_tweezers], dtype=object)
     multicounts_result_datafield_name_list = []
-    for tweezer_num, tweezer_roi in enumerate(tweezer_roi_list):
+    for tweezer_num in range(num_tweezers):
         for point_num in range(num_points):
-            roi_array[point_num, tweezer_num] = tweezer_roi
+            roi_array[point_num, tweezer_num] = \
+                make_centered_roi(vert_center=tweezer_00_vert_center + tweezer_num * tweezer_vert_spacing \
+                                              + point_num//4*piezo_vert_shift,
+                                    horiz_center=tweezer_00_horiz_center + tweezer_num * tweezer_horiz_spacing \
+                                                 + point_num//4*piezo_horiz_shift,
+                                    vert_span=tweezer_vert_span,
+                                    horiz_span=tweezer_horiz_span)
         new_counts_datafield = DataDictShotDataField(name=f'{frame_key}_tweezer-{tweezer_num:02d}_counts')
         datafield_list.append(new_counts_datafield)
         multicounts_result_datafield_name_list.append(new_counts_datafield.name)
@@ -88,16 +92,12 @@ for frame_num in range(num_frames):
                                                  roi_slice_array=roi_array)
     processor_list.append(multicounts_processor)
 
-# reporter_roi_dict = dict()
-# for frame_num in range(num_frames):
-#     datafield_name = f'frame-{frame_num:02d}_avg'
-#     reporter_roi_dict[datafield_name] = tweezer_roi_list
 
 avg_img_datafield_name_list = [f'frame-{frame_num:02d}_avg' for frame_num in range(num_frames)]
 image_point_reporter = ImagePointReporter(name='avg_frame_reporter',
                                           datafield_name_list=avg_img_datafield_name_list,
                                           layout=Reporter.LAYOUT_HORIZONTAL,
-                                          save_data=True, close_plots=True, roi_slice_array=roi_array)
+                                          save_data=True, close_plots=False, roi_slice_array=roi_array)
 
 datastream_list = [high_na_datastream]
 reporter_list += [image_point_reporter]
